333b3ff077854503b2097e645915ba55cf3d1f82
kostyawh <kostyawh@gmail.com>
2018-10-18 12:23:28 +0700

    Initial commit

---
+++ entity/Leave.py
@@ -0,0 +1,6 @@
+class Leave:
+
+    def __init__(self, value):
+        self.value = value
+        self.parent = None
+        self.children = []
---
+++ entity/SimpleTree.py
@@ -0,0 +1,7 @@
+class SimpleTree:
+
+    def __init__(self):
+        self.storage = None
+
+    def add_leave(self, leave):
+        pass
